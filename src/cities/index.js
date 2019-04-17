adc5e47d2f412ce5c850b1dcc3a5798b05df1c5a
Kapil Jugnu <kr_kapil@hotmail.com>
2019-04-17 11:10:23 +0530

    new component

---
+++ src/cities/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import styles from "./stylesheet";
+
+export default () => (
+  <View style={styles.container}>
+    <Text>Cities</Text>
+  </View>
+);
---
+++ src/login/stylesheet.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+/*
+* StyleSheet Object to create the style for the current screen
+* Flexbox work similar to css flexbox 
+*/
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
